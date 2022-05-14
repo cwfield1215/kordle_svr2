0f8547c12ada4cf16cea8f14b88672e637eb7110
Richard J. Field <Richard.Field@AlticeUSA.com>
2022-05-14 17:53:33 -0400

    fix: cleanups, including removing App Engine stuff

--- src/main/java/org/bmchsd/cwfield/kordle_svr/controller/KordleController.java
+++ src/main/java/org/bmchsd/cwfield/kordle_svr/controller/KordleController.java
@@ -1,9 +1,7 @@
 package org.bmchsd.cwfield.kordle_svr.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.bmchsd.cwfield.kordle_svr.model.KordleResult;
@@ -46,34 +43,4 @@ public class KordleController {
         resultService.saveOrUpdate(result);  
         return result.getId();  
     }  
-
-
-
-
-	//private static final String template = "Hello, %s!";
-	//private final AtomicLong counter = new AtomicLong();
-
-	// @GetMapping("/result/{user}")
-	// public KordleResult GetResultForId(@PathVariable(value = "user") String user) {
-	// 	return new KordleResult(counter.incrementAndGet(), user, 0, 1, "solved");
-	// }
-
-    // @GetMapping("/result")
-    // public @ResponseBody List<KordleResult> ListResults() {
-    //     KordleResult kr = new KordleResult(counter.incrementAndGet(), "chrfield", 0, 1, "solved");
-    //     ArrayList <KordleResult> krl = new ArrayList<KordleResult>();
-    //     krl.add(kr);
-    //     return krl;
-    // }
-
-    // @PostMapping("/result") 
-    // public KordleResult CreateResult(@RequestBody KordleResult kr) {
-    //     KordleResult kr2 = new KordleResult(
-    //         counter.incrementAndGet(),
-    //         kr.getUser(),
-    //         kr.getTimeMillis(),
-    //         kr.getNumTries(),
-    //         kr.getOutcome());
-    //     return kr2;
-    // }   
 }
--- src/main/java/org/bmchsd/cwfield/kordle_svr/model/KordleResult.java
+++ src/main/java/org/bmchsd/cwfield/kordle_svr/model/KordleResult.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 public class KordleResult {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    // //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
     private String user;
